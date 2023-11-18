070f5c75e3bfc7b14070f427e344704284a94e47
Keranod <konrad.konkel@wp.pl>
2023-11-18 07:25:30 +0000

    7.2

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,20 @@ const Menu = () => {
   }
   return (
     <div>
-        <Link style={padding} to='/'>anecdotes</Link>
+        <Link style={padding} to='/anecdotes'>anecdotes</Link>
         <Link style={padding} to='/create'>create new</Link>
         <Link style={padding} to='/about'>about</Link>
+    </div>
+  )
+}
+
+const Anecdote = ({ anecdote }) => {
+
+  return (
+    <div>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <div>has {anecdote.votes} votes</div><br />
+      <div>for more info see <a href={anecdote.info}>{anecdote.info}</a></div><br />
     </div>
   )
 }
@@ -27,7 +38,11 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
   </div>
 )
@@ -132,14 +147,22 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useMatch('/anecdotes/:id')
+
+  const anecdote = match
+    ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id))
+    : null
+
   return (
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
       <Routes>
-        <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote} />} />
+        <Route path='/anecdotes' element={<AnecdoteList anecdotes={anecdotes} />} />
         <Route path='/create' element={<CreateNew addNew={addNew} />} />
         <Route path='/about' element={<About />} />
+        <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
       </Routes>
       <Footer />
     </div>
